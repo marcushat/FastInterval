8237da9d88637bebaef76f81b8ba5a850b887bd5
marcushat <not_actual_email@yahoo.com>
2016-02-27 08:35:24 -0500

    initial commit

---
+++ fast_interval.js
@@ -0,0 +1,45 @@
+var FastInterval = function(onFastInterval, freqMicroseconds, onInterval) {
+	onInterval = typeof onInterval == 'undefined' ? function(){return true;} : onInterval;
+
+	var minInterval = 16000;
+	var intervalFreq = Math.max(minInterval, freqMicroseconds);
+
+	var burstSize = 1;
+	if(freqMicroseconds < minInterval) {
+		burstSize = freqMicroseconds ? (minInterval / freqMicroseconds) : minInterval * 10;
+		burstSize =  Math.max(1, burstSize);
+	}
+
+	var stop = false; //default
+
+	var interval = setInterval(function() {
+		if(!stop) {
+			stop = !onInterval();
+		}
+		if(stop) {
+			clearInterval(interval);
+		} else {
+			for(var i = 0; i < burstSize; i++) {
+				stop = !onFastInterval();
+				if(stop) {
+					break;
+				};
+			}
+		}
+	}, intervalFreq/1000);
+
+	this.clear = this.stop = function(){
+		stop = true;
+	}
+};
+
+//to make it a drop-in replacement for setInterval() and clearInterva()
+var setFastInterval = function(onFastInterval, freqMilliseconds, onInterval) {
+	var freqMicroseconds = freqMilliseconds * 1000;
+	var fastInterval = new FastInterval(onFastInterval, freqMicroseconds, onInterval);
+	return fastInterval;
+}
+
+var clearFastInterval = function(fastInterval) {
+	fastInterval.stop();
+}
